7d57f79f4a08b2ddefbaf752e24d7f79dbfd4296
Zachary Robbins <xaqiri@outlook.com>
2016-09-13 17:40:12 -0500

    Improved graphical display
    Graphical display now more acurately displays how the underlying
    algorithm works

--- sorting/bubbleSort/main.py
+++ sorting/bubbleSort/main.py
@@ -15,31 +15,41 @@ YELLOW = (255, 255, 0)
 COLORS = [] 
 w_s = w_w, w_h = 1600, 640 
 screen = p.display.set_mode(w_s) 
-num_bars = 145 
+num_bars = 144 
 bar_height = 200 
 
-def bubble_sort(l): 
-    for o in range(len(l)-1, 1, -1): 
-        for i in range(0, o): 
-            if l[i].height > l[i+1].height: 
-                t1 = l[i] 
-                t2 = l[i+1] 
-                l[i] = t2 
-                l[i+1] = t1 
-        break 
+def bubble_sort(l, i): 
+    if l[i].height > l[i+1].height: 
+        t1 = l[i] 
+        t2 = l[i+1] 
+        l[i] = t2 
+        l[i+1] = t1 
+    
+def render(bars): 
+    screen.fill(BLACK) 
+    for bar in bars: 
+        bar.render(screen) 
+    p.display.flip() 
             
 def create_array(): 
     return [Bar(random.randint(1, bar_height)*3, w_h-10, random.choice(COLORS)) for i in range(num_bars)] 
-    
+
 def create_colors(): 
     global COLORS 
-    COLORS = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for i in range(5)] 
+    for i in range(num_bars): 
+        c = (random.randint(50, 250), random.randint(50, 250), random.randint(50, 250)) 
+        if not c in COLORS: 
+            COLORS.append(c) 
+        else: 
+            c = (random.randint(50, 250), random.randint(50, 250), random.randint(50, 250)) 
 
 create_colors() 
 bars = create_array() 
 clock = p.time.Clock() 
 done = False 
 s = 1 
+o = num_bars - 1 
+i = 0 
 while not done: 
     p.event.pump() 
     for e in p.event.get(): 
@@ -52,18 +62,25 @@ while not done:
                 create_colors() 
                 bars = create_array() 
                 s = 1 
-                
+                o = num_bars - 1 
+                i = 0 
     screen.fill(BLACK) 
     for b in range(len(bars)): 
-        bars[b].render(screen, ((bars[b].width+1)*b))
+        bars[b].render(screen, 11*b) 
     p.display.flip() 
+    
     if s == 1: 
         time.sleep(0.5) 
-    else: 
-        time.sleep(0.1) 
-    bubble_sort(bars) 
+    #else: 
+        #time.sleep(0.1) 
+    if o > 1 and i < o: 
+        bubble_sort(bars, i) 
+        i += 1 
+    if i >= o: 
+        i = 0 
+        o -= 1 
     if s < 10: 
         s += 1 
-    clock.tick(60) 
+    #clock.tick(60) 
 
 p.quit() 
